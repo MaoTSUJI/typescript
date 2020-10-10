944b7b75e8f10e96e3f58769845a5448a06b658a
辻真緒 <>
2020-10-10 16:16:13 +0900

    study types

--- main.ts
+++ main.ts
@@ -6,7 +6,27 @@
 
 // Typescript
 var x: number = 10; // 一回型を決めるとあとから型を変更できない　変数のあとに: 型 を追記するだけ。
-x = "hello";
+// x = "hello";
 // 動的型付けだと大規模なプロジェクトにおいてエラーが発生しやすくなる。
 // 静的型付けにすることで安全なプログラムに変更する
 
+// 型
+
+/*
+number
+string
+boolean
+any
+ */
+
+ var i: number;
+ var i: number = 10;
+ var i = 10;    // number型になる（型推論）
+ 
+
+ var y; // var y: any
+ y = 10;
+ y = "hello";
+
+ var results: number[]; // 数値型の配列の書き方
+ results = [10, 5, 3];
